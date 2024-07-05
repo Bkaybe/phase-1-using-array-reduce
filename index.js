9c9eb325621437b53099b6d51f8f894be2e70bb0
Bethuel Khisa <sunguribethuel@gmail.com>
2024-07-05 09:32:08 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const calculateTotalBatteries = () => {
+  return batteryBatches.reduce((total, battery) => total + battery, 0);
+};
+
+const totalBatteries = calculateTotalBatteries();
